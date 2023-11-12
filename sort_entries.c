fa60060a910e7f34311e148dfded161c15c13748
Sigmanificient <edhyjox@gmail.com>
2023-11-12 20:16:15 +0100

    Optimize sort slightly

--- sort_entries.c
+++ sort_entries.c
@@ -9,17 +9,27 @@
 #include "quell/ql_debug.h"
 #include "quell/ql_string.h"
 
+inline
+void build_comp_str(char *restrict out, char *restrict s)
+{
+    for (; *s == '.'; s++);
+    ql_strcpy(out, s);
+    ql_strupcase(out);
+}
+
 static
 void reorganize_elements(entry_t *cmp_l, entry_t *cmp_r)
 {
     static entry_t swp;
-    static char left[PATH_MAX + 1];
+    static char left[NAME_MAX + 1];
     static char right[NAME_MAX + 1];
+    static entry_t *p = NULL;
 
-    ql_strcpy(left, cmp_l->name);
-    ql_strupcase(left);
-    ql_strcpy(right, cmp_r->name);
-    ql_strupcase(right);
+    build_comp_str(right, cmp_r->name);
+    if (p != cmp_l) {
+        build_comp_str(left, cmp_l->name);
+        p = cmp_l;
+    }
     if (ql_strcmp(left, right) < 0) {
         swp = *cmp_r;
         *cmp_r = *cmp_l;
